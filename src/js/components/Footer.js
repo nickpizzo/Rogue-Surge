5b52a63d1bc8d7b2dd7db93709ed4757967c3224
Nick Pizzo <nick.pizzo@gmail.com>
2016-11-21 18:22:29 -0800

    contact modal

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -7,7 +7,7 @@ import '../../css/partials/footer.scss';
 export default class Footer extends Component {
 	render() {
 		return (
-			<footer className="container-2">
+			<footer className="container-2" id="footer">
         <div className="footer-content">
           <div className="footer-icons">
             <FontAwesome className='footer-icon' size='2x' name='facebook-official'/>
@@ -15,8 +15,16 @@ export default class Footer extends Component {
             <FontAwesome className='footer-icon' size='2x' name='instagram'/>
             <FontAwesome className='footer-icon' size='2x' name='twitter'/>
           </div>
-          <div className="footer-contact">CONTACT</div>
+          <a href="#modal-container" className="footer-contact" aria-label="Open Navigation">CONTACT</a>
         </div>
+
+				<div id="modal-container">
+					<div className="modal">
+						<a href="#footer" className="modal-close" aria-label="Close Modal"><FontAwesome style={{ color: 'white' }} name='times'/></a>
+						<span>Email:&nbsp;</span><a href="mailto:contact@rogue.gg"><u>Contact@rogue.gg</u></a>
+
+					</div>
+				</div>
       </footer>
 		)
 	}
