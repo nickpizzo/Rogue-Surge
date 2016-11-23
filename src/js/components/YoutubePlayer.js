a7e9126c34bf298813c5ef6513fe32501f8d48c4
Nick Pizzo <nick.pizzo@gmail.com>
2016-11-23 15:49:43 -0800

    youtube player video playing bugfix

--- src/js/components/YoutubePlayer.js
+++ src/js/components/YoutubePlayer.js
@@ -14,16 +14,20 @@ export default class YoutubePlayer extends Component {
 		const {videoId} = this.props;
 
 		if (playing) return (
+			<div className="youtube-player">
 			<iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
 				frameBorder='0'
 				allowFullScreen='1'
 				style={{width: '100%', height: '100%'}}/>
+			</div>
 		)
 
     return (
-			<div className="youtube-player" onClick={this.playVideo}>
-				<img src={`https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`} />
-				<div className="play" />
+			<div className="youtube-player" data-id={videoId} onClick={this.playVideo}>
+				<div data-id={videoId}>
+					<img src={`https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`} />
+					<div className="play" />
+				</div>
 			</div>
 		)
 	}
